77b20d160bf93bcb29211fd5f2c20dad297a3945
tgkoco <tgkoco@qq.com>
2020-12-18 10:03:58 +0800

    ✅  引入 谷歌字体信息

--- flutterDemo/6text.dart
+++ flutterDemo/6text.dart
@@ -1,4 +1,12 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// 声明文本样式
+const textStyle = const TextStyle(
+  fontFamily: 'Raleway',
+);
+// const TextStyle = const GoogleFonts.getFont('Lato');
 
 void main() => runApp(AppIndex());
 
@@ -24,6 +32,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TapGestureRecognizer _tapGestureRecognizer = new TapGestureRecognizer();
+
+  @override
+  void dispose() {
+    //用到GestureRecognizer的话一定要调用其dispose方法释放资源
+    _tapGestureRecognizer.dispose();
+    super.dispose();
+  }
+
+  void _onTop() {
+    setState(() {
+      print("点击...");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -48,15 +71,16 @@ class _HomePageState extends State<HomePage> {
             textScaleFactor: 1.5,
           ),
           Text(
-            "TextStyle",
-            textScaleFactor: 5,
+            "TextStyle style 样式",
+            textScaleFactor: 1.2,
+            style: TextStyle(color: Colors.blue),
           ),
           Text(
             "Hello world",
             textAlign: TextAlign.center,
             style: TextStyle(
-                color: Colors.blue,
-                fontSize: 18.0,
+                color: Colors.yellow,
+                fontSize: 10.0,
                 //该属性和Text的textScaleFactor都用于控制字体大小。但是有两个主要区别：
                 // fontSize可以精确指定字体大小，而textScaleFactor只能通过缩放比例来控制。
                 // textScaleFactor主要是用于系统字体大小设置改变时对Flutter应用字体进行全局调整，而fontSize通常用于单个文本，字体大小不会跟随系统字体大小变化。
@@ -64,9 +88,58 @@ class _HomePageState extends State<HomePage> {
                     1.2, // 该属性用于指定行高，但它并不是一个绝对值，而是一个因子，具体的行高等于fontSize*height
                 fontFamily:
                     "Courier", //由于不同平台默认支持的字体集不同，所以在手动指定字体时一定要先在不同平台测试一下
-                background: new Paint()..color = Colors.yellow,
+                background: Paint()..color = Colors.green,
                 decoration: TextDecoration.underline,
                 decorationStyle: TextDecorationStyle.dashed),
+          ),
+          Text(
+            "TextSpan 片段，对一个Text内容的不同部分按照不同的样式显示",
+            textScaleFactor: 1.2,
+            style: TextStyle(color: Colors.blue),
+          ),
+          Text.rich(TextSpan(
+            children: [
+              TextSpan(text: "Home: "),
+              TextSpan(
+                text: "https://gitee.com.tgkoco",
+                style: TextStyle(color: Colors.blue),
+                recognizer: _tapGestureRecognizer..onTap = _onTop,
+              ),
+            ],
+          )),
+          Text(
+            "DefaultTextStyle 设置下方的所有子组件的样式",
+            textScaleFactor: 1.2,
+            style: TextStyle(color: Colors.blue),
+          ),
+          DefaultTextStyle(
+            //1.设置文本默认样式
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 20.0,
+            ),
+            textAlign: TextAlign.start,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("test 1"),
+                Text("test 2"),
+                Text(
+                  "no default， 使用自己的样式信息",
+                  style: TextStyle(
+                      inherit: false, //2.不继承默认样式
+                      color: Colors.grey),
+                ),
+              ],
+            ),
+          ),
+          Text(
+            "style: GoogleFonts ",
+            style: GoogleFonts.lato(fontStyle: FontStyle.italic),
+          ),
+          Text(
+            "data",
+            style: textStyle,
           )
         ],
       ),
