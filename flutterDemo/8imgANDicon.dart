44302d5256d4dfa672d342a0bf738cfde48ceaee
gettg <tgkoco@qq.com>
2021-01-26 09:54:41 +0800

    ✨ icon 图标和 自定义图标导入

--- flutterDemo/8imgANDicon.dart
+++ flutterDemo/8imgANDicon.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
+import '6text.dart';
+
 void main() => runApp(ImgIconIndex());
 
 String GetTitle() {
@@ -81,6 +83,16 @@ class _IndexPageState extends State<IndexPage> {
                         return FitImage();
                       },
                     ),
+                  )),
+          RaisedButton(
+              child: Text("Icon 展示"),
+              onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return IconShow();
+                      },
+                    ),
                   ))
         ],
       ),
@@ -89,16 +101,98 @@ class _IndexPageState extends State<IndexPage> {
 
   void _showArgs() {
     Fluttertoast.showToast(msg: """
-const Image({
-  this.width, //图片的宽
-  this.height, //图片高度
-  this.color, //图片的混合色值
-  this.colorBlendMode, //混合模式
-  this.fit,//缩放模式
-  this.alignment = Alignment.center, //对齐方式
-  this.repeat = ImageRepeat.noRepeat, //重复方式
-})
-""");
+                        const Image({
+                          this.width, //图片的宽
+                          this.height, //图片高度
+                          this.color, //图片的混合色值
+                          this.colorBlendMode, //混合模式
+                          this.fit,//缩放模式
+                          this.alignment = Alignment.center, //对齐方式
+                          this.repeat = ImageRepeat.noRepeat, //重复方式
+                        })
+                        """);
+  }
+}
+
+class IconShow extends StatefulWidget {
+  IconShow({Key key}) : super(key: key);
+
+  @override
+  _IconShowState createState() => _IconShowState();
+}
+
+// icon 矢量图片
+class _IconShowState extends State<IconShow> {
+  String getIcon() {
+    String icons = "图标和文字混合输出：";
+// accessible: &#xE914; or 0xE914 or E914
+    icons += "\uE914";
+// error: &#xE000; or 0xE000 or E000
+    icons += " \uE000";
+// fingerprint: &#xE90D; or 0xE90D or E90D
+    icons += " \uE90D";
+    return icons;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(
+        children: [
+          Text(
+            getIcon(),
+            style: TextStyle(
+                fontFamily: "MaterialIcons",
+                fontSize: 24.0,
+                color: Colors.green),
+          ),
+          // Text(
+          //   "icon 的组件",
+          //   style: TextStyle(
+          //     color: Colors.white70,
+          //     fontSize: 50,
+          //   ),
+          // ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "icon 组件的填充数据：",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.cyanAccent,
+                ),
+              ),
+              Icon(
+                Icons.accessible,
+                color: Colors.green,
+                size: 70,
+              ),
+              Icon(
+                Icons.error,
+                color: Colors.green,
+              ),
+              Icon(
+                Icons.fingerprint,
+                color: Colors.green,
+              ),
+            ],
+          ),
+          Text("自己添加图标库： 操作步骤，iconfont 添加图标到项目，进行下载 ttf 格式"),
+          Text("外部资源的加载需要重新打包，颜色都会消失"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(MyIcons.frock1, color: Colors.purple),
+              Icon(MyIcons.panties1, color: Colors.green),
+              Icon(MyIcons.pants1, color: Colors.purple),
+              Icon(MyIcons.stockings1, color: Colors.green),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
 
@@ -184,4 +278,48 @@ class FitImage extends StatelessWidget {
       ),
     );
   }
+}
+
+// {
+//   "icon_id": "19219569",
+//   "name": "上衣-01",
+//   "font_class": "shangyi-01",
+//   "unicode": "e610",
+//   "unicode_decimal": 58896
+// },
+// {
+//   "icon_id": "19219770",
+//   "name": "短裤-02",
+//   "font_class": "shangyi-02",
+//   "unicode": "e611",
+//   "unicode_decimal": 58897
+// },
+// {
+//   "icon_id": "19219771",
+//   "name": "内裤-03",
+//   "font_class": "shangyi-03",
+//   "unicode": "e612",
+//   "unicode_decimal": 58898
+// },
+// {
+//   "icon_id": "19219790",
+//   "name": "袜子-04",
+//   "font_class": "shangyi-04",
+//   "unicode": "e613",
+//   "unicode_decimal": 58899
+// }
+
+class MyIcons {
+  // 上衣-01
+  static const IconData frock1 =
+      const IconData(0xe610, fontFamily: 'myIcon', matchTextDirection: true);
+  // 短裤-02
+  static const IconData pants1 =
+      const IconData(0xe611, fontFamily: 'myIcon', matchTextDirection: true);
+  // 内裤-03
+  static const IconData panties1 =
+      const IconData(0xe612, fontFamily: 'myIcon', matchTextDirection: true);
+  //袜子-04
+  static const IconData stockings1 =
+      const IconData(0xe613, fontFamily: 'myIcon', matchTextDirection: true);
 }
